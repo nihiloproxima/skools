69ee3be146a34c205f7e0c4ac0b8a766404e735c
Nathan <nathanplouvier60@gmail.com>
2024-04-19 19:57:00 +0200

    ✨ Improve display

--- src/routes/count/+page.svelte
+++ src/routes/count/+page.svelte
@@ -1,21 +1,20 @@
 <script lang="ts">
-	import { getToastStore, type ToastSettings } from '@skeletonlabs/skeleton';
 	import diacritics from 'diacritics';
 
-	const toastStore = getToastStore();
-
-	const t: ToastSettings = {
-		message: 'Copié dans le presse-papier !'
-	};
-
 	let input = '';
-	let splitted = [];
+	let result: Array<{
+		line: string;
+		parts: Array<string>;
+		total: number;
+		cumulative: number;
+	}> = [];
+	let total = 0;
 
 	const count = () => {
-		const lines = input.split('\n');
+		total = 0;
+		result = [];
 
-		let total = 0;
-		let result = '';
+		const lines = input.split('\n');
 
 		for (const line of lines) {
 			// split lines by spaces
@@ -25,20 +24,18 @@
 				.remove(line)
 				.replace(/[^0-9a-zA-Z]+/g, ' ')
 				.trim();
-			console.log(formatted);
+
 			const parts = formatted.split(/\s+/);
-			console.log(parts);
+
+			result.push({
+				line,
+				parts,
+				total: parts.length,
+				cumulative: total + parts.length
+			});
 
 			total += parts.length;
-			const newLine = `${line} | ${total}\n`;
-
-			result += newLine;
-			console.log(newLine);
 		}
-
-		input = result;
-		navigator.clipboard.writeText(input);
-		toastStore.trigger(t);
 	};
 </script>
 
@@ -55,8 +52,39 @@
 				rows="10"
 				placeholder="Lorem ipsum dolor sit amet consectetur adipisicing elit."
 				bind:value={input}
+				on:keyup={count}
 			/>
-			<button class="btn variant-filled mt-5" on:click={count}>Compter</button>
 		</div>
+
+		<!-- Result -->
+		{#if result.length > 0}
+			<div class="mt-10">
+				<h3 class="h3">
+					Résultat: {total} mots sur {input.split('\n').filter((l) => l.length > 0).length} lignes.
+				</h3>
+
+				<table class="table table-compact table-hover table-auto text-left mt-5">
+					<thead>
+						<tr>
+							<th>Ligne</th>
+							<th class="table-cell-fit">Mots</th>
+							<th class="table-cell-fit">Cumulés</th>
+						</tr>
+					</thead>
+					<tbody>
+						{#each result as { line, parts, total, cumulative }, i}
+							<tr>
+								<td class="w-[90%]">
+									<p class="text-lg font-bold">{line}</p>
+									<p class="text-xs opacity-55">{parts.join(' | ')}</p>
+								</td>
+								<td class="font-bold text-lg table-cell-fit">{total}</td>
+								<td class="font-bold text-lg table-cell-fit">{cumulative}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</div>
+		{/if}
 	</div>
 </div>
